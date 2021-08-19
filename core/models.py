8b4fc06da092710938efe01fcde1014e66921432
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-19 16:23:42 +0100

    About Me model created

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# About Me Model
+
+
+class About(models.Model):
+    short_description = models.TextField()
+    description = models.TextField()
+    image = models.ImageField(upload_to="about")
+
+    class Meta:
+        verbose_name = "About me"
+        verbose_name_plural = "About me"
+
+    def __str__(self):
+        return "About me"
