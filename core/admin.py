5c73611d8f77a7594fc775d3054d0ced7429fd89
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-20 10:18:46 +0100

    sections About, Service, Work Experience and clien updated

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .views import *
 
-# Register your models here.
+admin.site.register(About)
+admin.site.register(Service)
+admin.site.register(WorkExperience)
+admin.site.register(Client)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
 
-# Create your views here.
+
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+    # override get context date method
+    def get_context_data(self, **kwargs):
+        # first, call super get context data
+        context = super().get_context_data(**kwargs)
+        context['about'] = About.objects.first()
+        context['services'] = Service.objects.all()
+        context['works'] = WorkExperience.objects.all()
+        return context
