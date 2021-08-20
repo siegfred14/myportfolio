6483b621575a7100f08b05400c8f4d56142bcad7
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-20 12:08:19 +0100

    views updated for contacts

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import *
+from django.http import HttpResponse
 
 
 class HomeTemplateView(TemplateView):
@@ -13,5 +15,21 @@ class HomeTemplateView(TemplateView):
         context['about'] = About.objects.first()
         context['services'] = Service.objects.all()
         context['portfolio'] = WorkExperience.objects.all()
-        context['contact'] = Contact.objects.all()
+        # context['contact'] = Contact.objects.all()
         return context
+
+
+def index(request):
+    if request.method == "POST":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        contact.name = name
+        contact.email = email
+        contact.message = message
+
+        contact.save()
+        return HttpResponse("Thanks For Contacting Me. You'll receive a feed back in 18hrs!")
+    return render(request, 'home.html')
