be1cc3a83fd9d707771b282c73bbfcd52867b051
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-27 23:30:34 +0100

    email backend added

--- monresume/settings.py
+++ monresume/settings.py
@@ -31,6 +31,7 @@ DEBUG = False
 # EMAIL_HOST_PASSWORD = 'to be added'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'portfoliosiegfred.herokuapp.com']
 
